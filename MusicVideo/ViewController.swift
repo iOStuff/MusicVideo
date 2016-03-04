a19e88cd42af8637ffd3affb166c424e246b037e
David Goldberg <dgoldberg@steelbrick.com>
2016-03-04 00:22:10 -0800

    Adds logic for ReachStatusChanged observer

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
+        
         // Call API
         let api = APIManager()
         api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json",
             completion: didLoadData)
     }
+    
+    @IBOutlet weak var displayLabel: UILabel!
     
     func didLoadData(videos: [Videos]) {
         
@@ -33,6 +39,24 @@ class ViewController: UIViewController {
             print("\(index) name = \(item.vName)")
         }
         
+    }
+    
+    func reachabilityStatusChanged() {
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLabel.text = "Reachable with Cellular"
+        default:return
+        }
+    }
+    
+    // Called just as the object is about to be deallocated
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
 
 }
